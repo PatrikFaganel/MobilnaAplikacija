3881393ddbb057f205e6e7241ad550b66984ca10
Patrik Faganel <pat3k.fag@gmail.com>
2021-08-23 22:37:48 +0200

    Add project files.

---
+++ MobilnaAplikacija/Model/astronautData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilnaAplikacija.Model
+{
+
+    public class Rootobject
+    {
+        public string title { get; set; }
+        public List<Variable> variables { get; set; }
+    }
+
+    public class Variable
+    {
+        public string code { get; set; }
+        public string text { get; set; }
+        public string[] values { get; set; }
+        public string[] valueTexts { get; set; }
+        public bool elimination { get; set; }
+    }
+
+}
---
+++ MobilnaAplikacija/SecondPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using MobilnaAplikacija.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace MobilnaAplikacija
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SecondPage : Page
+    {
+        public SecondPage()
+        {
+            this.InitializeComponent();
+
+            this.btn_GoBack.Click += Btn_GoBack_Click;
+        }
+
+        private void Btn_GoBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Frame.CanGoBack)
+            {
+                this.Frame.GoBack();
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (string.IsNullOrEmpty(e.Parameter.ToString()))
+            {
+                this.txtB_FromMainPage.Text = "No name!!!";
+            }
+            else
+            {
+                this.txtB_FromMainPage.Text = e.Parameter.ToString();
+            }
+        }
+        async void getData()
+        {
+            string url = "https://w3.unece.org/PXWeb2015/api/v1/en/STAT/10-CountryOverviews/01-Figures/ZZZ_en_CoSummary_r.px";
+
+            HttpClient client = new HttpClient();
+
+            string response = await client.GetStringAsync(url);
+
+            var data = JsonConvert.DeserializeObject<Rootobject>(response);
+
+            result_lbl.Text = data.ToString();
+        }
+    }
+}
